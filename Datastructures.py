26e1ba7fe44a622e0e718e5b3d1e6207d36b5017
tolo017 <otibomb98@gmail.com>
2025-03-22 10:01:40 +0300

    Add files via upload

---
+++ Datastructures.py
@@ -0,0 +1,23 @@
+# Create an empty list
+my_list = []
+
+# Append elements 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert 15 at the second position
+my_list.insert(1, 15)
+
+# Extend the list with [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+# Remove the last element
+my_list.pop()
+
+# Sort in ascending order
+my_list.sort()
+
+# Find and print the index of 30
+print("Index of 30:", my_list.index(30))
